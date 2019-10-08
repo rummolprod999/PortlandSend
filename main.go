4cecf8d78a6b5ff91ecd086e2438ff29ba7ae1fd
rummolprod999 <rummolprod999@gmail.com>
2019-10-08 09:03:46 +0300

    update

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ func init() {
 func main() {
 	defer SaveStack()
 	Logging("Start work")
-	p := ParserEis{}
-	p.run()
+	/*p := ParserEis{}
+	p.run()*/
 	c := ParserComita{}
 	c.run()
 	d := ParserEisNew{}
 	d.run()
-	Logging(fmt.Sprintf("Add purchases %d", p.addDoc))
-	Logging(fmt.Sprintf("Send purchases %d", p.sendDoc))
+	/*Logging(fmt.Sprintf("Add purchases %d", p.addDoc))
+	Logging(fmt.Sprintf("Send purchases %d", p.sendDoc))*/
 	Logging(fmt.Sprintf("Add purchases new eis %d", d.addDoc))
 	Logging(fmt.Sprintf("Send purchases new eis %d", d.sendDoc))
 	Logging(fmt.Sprintf("Add purchases comita %d", c.addDoc))
